27e9c7887742f1c39628f8a1e25526c851312e65
fawez9 <hattabifawez99@gmail.com>
2024-05-06 19:59:31 +0100

    Verifying kosaka's work and adding the pdf to file upload

--- components/ui/file-upload.tsx
+++ components/ui/file-upload.tsx
@@ -1,4 +1,4 @@
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
@@ -11,29 +11,41 @@ interface FileUploadProps {
 }
 
 export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
-  const [imageUrl, setImageUrl] = useState<string | undefined>(value);
+  const [fileUrl, setFileUrl] = useState<string | undefined>(value);
+  const [fileType, setFileType] = useState<string | undefined>();
 
-  const handleUploadComplete = (res: { url: string }[]) => {
+  const handleUploadComplete = (res: { url: string; type: string }[]) => {
     onChange(res?.[0]?.url);
-    console.log("Files: ", res);
-    setImageUrl(res[0].url);
+    setFileUrl(res[0].url);
+    setFileType(res[0].type);
   };
 
   const handleUploadError = (error: Error) => {
     console.log(error);
   };
 
+  const removeFile = () => {
+    onChange("");
+    setFileUrl(undefined);
+    setFileType(undefined);
+  };
+
   return (
     <>
-      {imageUrl ? (
+      {fileUrl && (fileType === "image/png" || fileType === "image/jpeg") ? (
         <div className="relative h-20 w-20">
-          <Image src={imageUrl} alt="Upload" className="rounded-full" layout="fill" objectFit="cover" />
-          <button
-            onClick={() => {
-              onChange("");
-              setImageUrl(undefined);
-            }}
-            className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm ">
+          <Image src={fileUrl} alt="Upload" className="rounded-full" layout="fill" objectFit="cover" />
+          <button onClick={removeFile} className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm">
+            <X className="h-4 w-4" />
+          </button>
+        </div>
+      ) : fileUrl && fileType === "application/pdf" ? (
+        <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+          <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400 " />
+          <a href={fileUrl} target="_blank" rel="noopener noreferrer" className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline">
+            {fileUrl}
+          </a>
+          <button onClick={removeFile} className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm">
             <X className="h-4 w-4" />
           </button>
         </div>
